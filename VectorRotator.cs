d4af43f4ad5e5bb4b789dc57b8634406a436d0b7
Dan Lodholm <dlodholm@icloud.com>
2021-11-06 14:49:30 -0500

    Version 2: Dot Product
    Identified errors of reversibility where rotation wouldn't perform correctly when reversed.
    Replaced original math with new dot product math that is reversible and accurate under much more drastic rotations.

--- VectorRotator.cs
+++ VectorRotator.cs
@@ -1,43 +1,17 @@
 public static class VectorRotator {
-    public static Vector3 RotateVector(Vector3 vector, Vector3 euler)
+    public static Vector3 RotateVector (Vector3 vector, Vector3 euler)
     {
-        float X = vector.x;
-        float Y = vector.y;
-        float Z = vector.z;
+        Matrix4x4 m = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(euler), Vector3.one);
+        Vector3 mForward = m.MultiplyVector(Vector3.forward);
+        Vector3 mRight = m.MultiplyVector(Vector3.right);
+        Vector3 mUp = m.MultiplyVector(Vector3.up);
 
-        //Do rotations around Zfirst (like Unity does) - ROLL
-        float A = euler.z * Mathf.Deg2Rad;
-        //Then around X (like Unity) - PITCH
-        float B = euler.x * Mathf.Deg2Rad;
-        //Then around Y (like Unity) - YAW
-        float C = euler.y * Mathf.Deg2Rad;
+        Vector3 dotted = new Vector3(
+            Vector3.Dot(vector, mRight),
+            Vector3.Dot(vector, mUp),
+            Vector3.Dot(vector, mForward)
+        );
 
-        //Get our Sine and Cosine values stored for potential debugging and easy access
-        float cosA = Mathf.Cos(A);
-        float sinA = Mathf.Sin(A);
-
-        float cosB = Mathf.Cos(B);
-        float sinB = Mathf.Sin(B);
-
-        float cosC = Mathf.Cos(C);
-        float sinC = Mathf.Sin(C);
-
-        //Perform the rotations, or rather, calculate the values of each axis after rotating (same thing, right?)
-        float fX =
-            X * (cosA * cosC + sinA * sinB * sinC) +
-            Y * (cosA * sinB * sinC - sinA * cosC) +
-            Z * cosB * sinC;
-
-        float fY =
-            X * (sinA * cosB) +
-            Y * (cosA * cosB) -
-            Z * sinB;
-
-        float fZ =
-            X * (sinA * sinB * cosC - cosA * sinC) +
-            Y * (sinA * sinC + cosA * sinB * cosC) +
-            Z * (cosB * cosC);
-
-        return new Vector3(fX, fY, fZ);
+        return dotted;
     }
 }
